353bb4d7405b080fa92c29dbfd6f7f3e8306a40b
artem <zaitsev998@mail.ru>
2020-11-26 01:37:02 +0300

    simple sql

--- database/prepare_db.py
+++ database/prepare_db.py
@@ -1,43 +1,53 @@
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
-from models import Base, TestUser
+import pymysql
+from pymysql.cursors import DictCursor
 
 
-class MysqlOrmConnection(object):
+class MysqlConnection(object):
 
-    def __init__(self, user, password, db_name, host, port):
+    def __init__(self, user, password, db_name, host, port, charset='utf8'):
         self.user = user
         self.password = password
         self.db_name = db_name
-        self.port = port
         self.host = host
+        self.port = port
+        self.charset = charset
         self.connection = self.connect()
-        session = sessionmaker(bind=self.connection)
-        self.session = session()
 
     def get_connection(self, db_created=False):
-        engine = sqlalchemy.create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{db}'.format(
-            user=self.user,
-            password=self.password,
-            host=self.host,
-            port=self.port,
-            db=self.db_name if db_created else ''
-        ))
-        return engine.connect()
+        return pymysql.connect(host=self.host, port=self.port, user=self.user, password=self.password,
+                               db=self.db_name if db_created else None,
+                               charset=self.charset, cursorclass=DictCursor, autocommit=True)
 
     def connect(self):
-        connection = self.get_connection(db_created=False)
+        connection = self.get_connection()
 
-        connection.execute(f'DROP DATABASE IF EXISTS {self.db_name}')
-        connection.execute(f'CREATE DATABASE {self.db_name}')
-
+        connection.query(f'DROP DATABASE IF EXISTS {self.db_name}')
+        connection.query(f'CREATE DATABASE {self.db_name}')
         connection.close()
 
         return self.get_connection(db_created=True)
 
+    def execute_query(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+
 
 if __name__ == '__main__':
-    connection = MysqlOrmConnection('root', 'pass', 'MYSQL_DB', host='0.0.0.0', port=3306)
-    engine = connection.connection.engine
-    if not engine.dialect.has_table(engine, 'test_users'):
-        Base.metadata.tables['test_users'].create(engine)
+    connection = MysqlConnection('root', 'pass', 'MYSQL_DB', host='0.0.0.0', port=3306)
+    query = """
+        CREATE TABLE `test_users` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `username` varchar(16) DEFAULT NULL,
+        `password` varchar(255) NOT NULL,
+        `email` varchar(64) NOT NULL,
+        `access` smallint DEFAULT NULL,
+        `active` smallint DEFAULT NULL,
+        `start_active_time` datetime DEFAULT NULL,
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `email` (`email`),
+        UNIQUE KEY `ix_test_users_username` (`username`)
+    )
+    """
+    connection.execute_query(query)
+
