cf2d3f5a2258d51c5e097c2f8e67d21f940dd864
artem <zaitsev998@mail.ru>
2020-11-26 00:00:37 +0300

    db prepairing

---
+++ database/prepare_db.py
@@ -0,0 +1,43 @@
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from models import Base, TestUser
+
+
+class MysqlOrmConnection(object):
+
+    def __init__(self, user, password, db_name, host, port):
+        self.user = user
+        self.password = password
+        self.db_name = db_name
+        self.port = port
+        self.host = host
+        self.connection = self.connect()
+        session = sessionmaker(bind=self.connection)
+        self.session = session()
+
+    def get_connection(self, db_created=False):
+        engine = sqlalchemy.create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{db}'.format(
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            db=self.db_name if db_created else ''
+        ))
+        return engine.connect()
+
+    def connect(self):
+        connection = self.get_connection(db_created=False)
+
+        connection.execute(f'DROP DATABASE IF EXISTS {self.db_name}')
+        connection.execute(f'CREATE DATABASE {self.db_name}')
+
+        connection.close()
+
+        return self.get_connection(db_created=True)
+
+
+if __name__ == '__main__':
+    connection = MysqlOrmConnection('root', 'pass', 'MYSQL_DB', host='127.0.0.1', port=3333)
+    engine = connection.connection.engine
+    if not engine.dialect.has_table(engine, 'test_users'):
+        Base.metadata.tables['test_users'].create(engine)
